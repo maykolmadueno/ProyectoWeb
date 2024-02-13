3469d4bffd416f23915ee2a7f80fd1c79256e232
Victor Ormeño <20100835@ue.edu.pe>
2024-02-13 00:22:43 -0500

    Se agrega componentes de confirmacion

---
+++ UESAN/src/components/Modal/ModalC.vue
@@ -0,0 +1,69 @@
+<!-- Modal.vue -->
+<template>
+  <div class="modal" v-if="mostrar">
+    <div class="modal-content">
+      <slot></slot>
+      <div class="modal-actions">
+        <button @click="confirmar">Confirmar</button>
+        <button @click="cancelar">Cancelar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    mostrar: {
+      type: Boolean,
+      required: true
+    },
+    onConfirmar: {
+      type: Function,
+      required: true
+    },
+    onCancelar: {
+      type: Function,
+      required: true
+    }
+  },
+  methods: {
+    confirmar() {
+      this.onConfirmar();
+    },
+    cancelar() {
+      this.onCancelar();
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos del modal (puedes personalizarlos según tus necesidades) */
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+.modal-actions {
+  margin-top: 20px;
+  text-align: right;
+}
+
+button {
+  margin-left: 10px;
+}
+</style>
---
+++ UESAN/src/components/Modal/ModalNuevaContra.vue
@@ -0,0 +1,75 @@
+<!-- CambiarContrasenaModal.vue -->
+<template>
+  <div class="modal" v-if="mostrar">
+    <div class="modal-content">
+      <h2>Cambiar contraseña</h2>
+      <input type="password" v-model="nuevaContrasena" placeholder="Nueva contraseña">
+      <div class="modal-actions">
+        <button @click="confirmar">Confirmar</button>
+        <button @click="cancelar">Cancelar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    mostrar: {
+      type: Boolean,
+      required: true
+    },
+    onConfirmar: {
+      type: Function,
+      required: true
+    },
+    onCancelar: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      nuevaContrasena: ''
+    };
+  },
+  methods: {
+    confirmar() {
+      this.onConfirmar(this.nuevaContrasena);
+    },
+    cancelar() {
+      this.onCancelar();
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos del modal (puedes personalizarlos según tus necesidades) */
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+.modal-actions {
+  margin-top: 20px;
+  text-align: right;
+}
+
+button {
+  margin-left: 10px;
+}
+</style>
